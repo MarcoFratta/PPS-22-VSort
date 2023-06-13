dcbe2709e23ddeb9eaa566dcf5321694c29d4ad1
marco <marco.frattarola@studio.unibo.it>
2023-06-13 16:22:44 +0200

    Added type of index in Sortable

--- src/main/scala/model/Sortable.scala
+++ src/main/scala/model/Sortable.scala
@@ -4,43 +4,47 @@ import scala.util.{Failure, Success, Try}
 
 
 trait Sortable[T]:
-  def swap(a: Int, b: Int): Try[Sortable[T]]
-  
-  def select(a: Int): Try[Sortable[T]]
 
-  def deselect(a: Int): Try[Sortable[T]]
+  type Index = Int
 
-  def compare(a: Int, b: Int)(ifTrue: Sortable[T] => Sortable[T])
+  def swap(a: Index, b: Index): Try[Sortable[T]]
+
+  def select(a: Index): Try[Sortable[T]]
+
+  def deselect(a: Index): Try[Sortable[T]]
+
+  def compare(a: Index, b: Index)(ifTrue: Sortable[T] => Sortable[T])
              (ifFalse: Sortable[T] => Sortable[T])
              (using f: (T, T) => Boolean): Try[Sortable[T]]
   def length(): Int
 object Sortable:
-  def apply[T](seq: Seq[T], step: Seq[Step]): Sortable[T] = new StepList[T](seq, step)
-  def apply[T](): Sortable[T] = new StepList[T](Seq.empty, Seq.empty)
-  def apply[T](seq:T*): Sortable[T] = new StepList[T](seq, Seq.empty)
 
-private case class StepList[T](data: Seq[T], steps: Seq[Step]) extends Sortable[T]:
+  def apply[T](seq: Seq[T], step: Seq[Step]): Sortable[T] = new SteppedList[T](seq, step)
+  def apply[T](): Sortable[T] = new SteppedList[T](Seq.empty, Seq.empty)
+  def apply[T](seq:T*): Sortable[T] = new SteppedList[T](seq, Seq.empty)
 
-  def swap(a: Int, b: Int): Try[Sortable[T]] =
-    Try(StepList(swapElements(a, b), addStep(Step.Swap(a, b))))
+private case class SteppedList[T](data: Seq[T], steps: Seq[Step]) extends Sortable[T]:
 
-  def select(a: Int): Try[Sortable[T]]=
-    Try(StepList(data, addStep(Step.Selection(a))))
+  def swap(a: Index, b: Index): Try[Sortable[T]] =
+    Try(SteppedList(swapElements(a, b), addStep(Step.Swap(a, b))))
 
-  def deselect(a: Int): Try[Sortable[T]] =
-    Try(StepList(data, addStep(Step.Deselection(a))))
+  def select(a: Index): Try[Sortable[T]]=
+    Try(SteppedList(data, addStep(Step.Selection(a))))
 
-  def compare(a: Int, b: Int)(ifTrue: Sortable[T] => Sortable[T])
+  def deselect(a: Index): Try[Sortable[T]] =
+    Try(SteppedList(data, addStep(Step.Deselection(a))))
+
+  def compare(a: Index, b: Index)(ifTrue: Sortable[T] => Sortable[T])
              (ifFalse: Sortable[T] => Sortable[T])
              (using f: (T, T) => Boolean): Try[Sortable[T]] =
     Try {
-      val memoryList = StepList(data, addStep(Step.Comparison(a, b)))
+      val memoryList = SteppedList(data, addStep(Step.Comparison(a, b)))
       if f(data(a), data(b)) then ifTrue(memoryList) else ifFalse(memoryList)
     }
 
   def length(): Int = data.size
 
-  private def swapElements(a: Int, b: Int): Seq[T] =
+  private def swapElements(a: Index, b: Index): Seq[T] =
     data updated(a, data.toList(b)) updated(b, data.toList(a))
 
   private def addStep(step: Step): Seq[Step] = steps :+ step
