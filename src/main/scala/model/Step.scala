2a950e751bfb1eb95fa765c92751b6898ddfdd6b
Alex Siroli <alex.siroli@studio.unibo.it>
2023-06-14 15:43:51 +0200

    edited selection and deselection steps

--- src/main/scala/model/Step.scala
+++ src/main/scala/model/Step.scala
@@ -2,7 +2,7 @@ package model
 
 enum Step:
     case Swap(a: Int, b: Int)
-    case Selection(a: Int)
-    case Deselection(a: Int)
+    case Selection(s: String, a: Int)
+    case Deselection(s: String)
     case Comparison(a: Int, b: Int)
 
