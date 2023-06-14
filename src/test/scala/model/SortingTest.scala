f061a64281eb2cdd662f2c7e606376b58c61c2f4
Alex Siroli <alex.siroli@studio.unibo.it>
2023-06-14 15:46:08 +0200

    added insertion sort test

--- src/test/scala/model/SortingTest.scala
+++ src/test/scala/model/SortingTest.scala
@@ -19,3 +19,16 @@ class SortingTest extends AnyFlatSpec with Matchers:
     }
     mList1.data shouldBe Seq(1, 2, 3, 4, 5)
   }
+
+  "Insertion sort" should "work" in {
+    var mList = Sortable(5, 4, 2, 1, 3)
+    for (i <- 0 to mList.length() - 2) {
+      mList = mList.select("min", i).get
+      for (j <- i + 1 until mList.length()) {
+        mList = mList.compare(mList.getSelection("min"), j)(x => x.select("min", j).get)(x => x)(using _ - _ > 0).get
+      }
+      mList = mList.swap(mList.getSelection("min"), i).get
+      mList = mList.deselect("min").get
+    }
+    mList.data shouldBe Seq(1, 2, 3, 4, 5)
+  }
