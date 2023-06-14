798c038e53346b3cc6ead7a4c1821130a1975697
Alex Siroli <alex.siroli@studio.unibo.it>
2023-06-14 16:37:03 +0200

    added selection sort test

--- src/test/scala/model/SortingTest.scala
+++ src/test/scala/model/SortingTest.scala
@@ -20,7 +20,7 @@ class SortingTest extends AnyFlatSpec with Matchers:
     mList1.data shouldBe Seq(1, 2, 3, 4, 5)
   }
 
-  "Insertion sort" should "work" in {
+  "Selection sort" should "work" in {
     var mList = Sortable(5, 4, 2, 1, 3)
     for (i <- 0 to mList.length() - 2) {
       mList = mList.select("min", i).get
@@ -28,7 +28,18 @@ class SortingTest extends AnyFlatSpec with Matchers:
         mList = mList.compare(mList.getSelection("min"), j)(x => x.select("min", j).get)(x => x)(using _ - _ > 0).get
       }
       mList = mList.swap(mList.getSelection("min"), i).get
-      mList = mList.deselect("min").get
+    }
+    mList.data shouldBe Seq(1, 2, 3, 4, 5)
+  }
+
+  "Insertion sort" should "work" in {
+    var mList = Sortable(5, 4, 2, 1, 3)
+    for (i <- 1 until mList.length()) {
+      mList = mList.select("sel", i).get
+      for (j <- i - 1 to 0 by -1) {
+        mList = mList.compare(j, mList.getSelection("sel"))(x => x
+          .swap(mList.getSelection("sel"), j).get.select("sel", j).get)(x => x)(using _ - _ > 0).get
+      }
     }
     mList.data shouldBe Seq(1, 2, 3, 4, 5)
   }
