04d475fcd20603b2abb4872dc15fd00eb81fa315
Alex Siroli <alex.siroli@studio.unibo.it>
2023-06-14 15:44:49 +0200

    added getSelection and implemented selectionMap in sortable

--- src/main/scala/model/Sortable.scala
+++ src/main/scala/model/Sortable.scala
@@ -9,9 +9,11 @@ trait Sortable[T]:
 
   def swap(a: Index, b: Index): Try[Sortable[T]]
 
-  def select(a: Index): Try[Sortable[T]]
+  def select(s: String, a: Index): Try[Sortable[T]]
 
-  def deselect(a: Index): Try[Sortable[T]]
+  def getSelection(s: String): Index
+
+  def deselect(s: String): Try[Sortable[T]]
 
   def compare(a: Index, b: Index)(ifTrue: Sortable[T] => Sortable[T])
              (ifFalse: Sortable[T] => Sortable[T])
@@ -24,25 +26,29 @@ trait Sortable[T]:
 
 object Sortable:
 
-  def apply[T](seq: Seq[T], step: Seq[Step]): Sortable[T] = new SteppedList[T](seq, step)
-  def apply[T](): Sortable[T] = new SteppedList[T](Seq.empty, Seq.empty)
-  def apply[T](seq:T*): Sortable[T] = new SteppedList[T](seq, Seq.empty)
+  def apply[T](seq: Seq[T], step: Seq[Step], map: Map[String, Int]): Sortable[T] = new SteppedList[T](seq, step, map)
+  def apply[T](seq: Seq[T], step: Seq[Step]): Sortable[T] = new SteppedList[T](seq, step, Map.empty)
+  def apply[T](): Sortable[T] = new SteppedList[T](Seq.empty, Seq.empty, Map.empty)
+  def apply[T](seq: T*): Sortable[T] = new SteppedList[T](seq, Seq.empty, Map.empty)
 
-private case class SteppedList[T](override val data: Seq[T], override val steps: Seq[Step]) extends Sortable[T]:
+private case class SteppedList[T](override val data: Seq[T], override val steps: Seq[Step], map: Map[String, Int]) extends Sortable[T]:
 
   def swap(a: Int, b: Int): Try[Sortable[T]] =
-    Try(SteppedList(swapElements(a, b), addStep(Step.Swap(a, b))))
-  def select(a: Int): Try[Sortable[T]] =
-    Try(SteppedList(data, addStep(Step.Selection(validIndex(a)))))
+    Try(SteppedList(swapElements(a, b), addStep(Step.Swap(a, b)), map))
 
-  def deselect(a: Int): Try[Sortable[T]] =
-    Try(SteppedList(data, addStep(Step.Deselection(validIndex(a)))))
+  def select(s: String, a: Int): Try[Sortable[T]] =
+    Try(SteppedList(data, addStep(Step.Selection(s, validIndex(a))), addSelection(s, a)))
+
+  def getSelection(s: String): Int = map(s)
+
+  def deselect(s: String): Try[Sortable[T]] =
+    Try(SteppedList(data, addStep(Step.Deselection(s)), deleteSelection(s)))
 
   def compare(a: Int, b: Int)(ifTrue: Sortable[T] => Sortable[T])
              (ifFalse: Sortable[T] => Sortable[T])
              (using f: (T, T) => Boolean): Try[Sortable[T]] =
     Try {
-      val memoryList = SteppedList(data, addStep(Step.Comparison(a, b)))
+      val memoryList = SteppedList(data, addStep(Step.Comparison(a, b)), map)
       if f(data(a), data(b)) then ifTrue(memoryList) else ifFalse(memoryList)
     }
 
@@ -52,6 +58,12 @@ private case class SteppedList[T](override val data: Seq[T], override val steps:
     data updated(a, data.toList(b)) updated(b, data.toList(a))
 
   private def addStep(step: Step): Seq[Step] = steps :+ step
+
+  private def addSelection(s: String, a: Int) : Map[String, Int] =
+    map updated(s, a)
+
+  private def deleteSelection(s: String): Map[String, Int] =
+    map removed s
 
   private def validIndex(index: Int): Int =
     if data.size > index then index else throw IllegalArgumentException()
