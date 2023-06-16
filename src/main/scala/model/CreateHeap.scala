427a0ea0d66d164188c44c31c67674675378e341
Alice Mastrilli <alice.mastrilli@studio.unibo.it>
2023-06-16 11:00:25 +0200

    changed implementation of heapify

--- src/main/scala/model/CreateHeap.scala
+++ src/main/scala/model/CreateHeap.scala
@@ -2,30 +2,19 @@ package model
 
 import scala.annotation.tailrec
 
+import scala.math.Ordering.Implicits._
 object CreateHeap:
-    def buildHeap(seq: Seq[Int]): Seq[Int] =
-      val size = seq.size
-      val start = (size / 2) - 1
-      (start to 0 by -1).foldLeft(seq) ( (acc, i) => heapify(acc, i, size))
 
-    @tailrec
-    private def heapify(seq: Seq[Int], i: Int, size: Int): Seq[Int] =
-      val left = 2 * i + 1
-      val right = 2 * i + 2
-      var largest = i
+  private def insert[T: Ordering](heap: Seq[T], newItem: T) = {
 
-      if (left < size && seq(left) > seq(largest))
-        largest = left
+    @annotation.tailrec
+    def siftUp(h: Seq[T], idx: Int): Seq[T] = {
+      val parentIdx = (idx - 1) >> 1
+      if (parentIdx < 0 || h(parentIdx) > h(idx)) h
+      else siftUp(h.updated(parentIdx, h(idx)).updated(idx, h(parentIdx)), parentIdx)
+    }
 
-      if (right < size && seq(right) > seq(largest))
-        largest = right
+    siftUp(heap :+ newItem, heap.length)
+  }
 
-      if (largest != i) {
-        val updatedSeq = swap(seq, i, largest)
-        heapify(updatedSeq, largest, size)
-      } else {
-        seq
-      }
-
-    private def swap(seq:Seq[Int], i: Int, j:Int):Seq[Int] =
-      seq.updated(i, seq(j)).updated(j, seq(i))
+  def heapify[T: Ordering](vs: Seq[T]): Seq[T] = vs.foldLeft(Seq.empty[T])(insert)
