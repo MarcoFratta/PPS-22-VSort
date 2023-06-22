9dd87bc8195c652c1518a506d0d31e6f323f0797
Alex Siroli <alex.siroli@studio.unibo.it>
2023-06-22 17:20:08 +0200

    insertion sort completed and merge sort in place

--- src/main/scala/model/SortingAlgorithms.scala
+++ src/main/scala/model/SortingAlgorithms.scala
@@ -1,6 +1,7 @@
 package model
 
 import model.Step.Comparison
+import model.sortModel.{SelectableM, Selections}
 import model.sortModel.SortOperation.*
 import model.sortModel.SortableFunctionalities.*
 import model.sortModel.SortableM
@@ -12,47 +13,68 @@ object SortingAlgorithms {
   given Comparable[Int] with
     override def compare(a: Int, b: Int): Boolean = a - b > 0
 
-  given Conversion[Steps[Int], SortOps[Steps[Int]]] = _ !
-
   def bubbleSort(seq: Seq[Int]): Seq[Step] =
+    given Conversion[Steps[Int], SortOps[Steps[Int]]] = _ !
+
     (for res <- SortableM(seq).iterate(0 to seq.length - 2)(
       (i, t) => t.iterate(0 to t.length - 2 - i)(
         (j, t2) => t2.compare(j, j + 1)(x => x.swap(j, j + 1))(x => x)))
     yield (res.data, res.steps)).get._2
 
 
-  def selectionSort(seq: Seq[Int]): Seq[Step] = ???
+  def selectionSort(seq: Seq[Int]): Seq[Step] =
+    given Conversion[Steps[Int] with Selections[String, Int], SortOps[Steps[Int] with Selections[String, Int]]] = _ !
 
-  def insertionSort(seq: Seq[Int]): Seq[Step] = ???
+    (for res <- SelectableM(seq).iterate(0 to seq.length - 2)(
+        (i, t1) => for p1 <- t1.select(" min", i)
+                       p2 <- p1.iterate(i + 1 until seq.length)(
+                         (j, t2) => t2.compare(t2.getSelection(" min").get, j)(x =>
+                           for p5 <- x.deselect(" min")
+                               p6 <- p5.select(" min", j)
+                           yield p6)
+                         (x => x)
+                       )
+                       p3 <- p2.swap(p2.getSelection(" min").get, i)
+                       p4 <- p3.deselect(" min")
+        yield p4
+      ) yield (res.data, res.steps)).get._2
 
-  def mergeSort(seq: Seq[Int]): Seq[Step] = ???
+//  def insertionSort(seq: Seq[Int]): Seq[Step] = ???
+//
+//  def mergeSort(seq: Seq[Int]): Seq[Step] = ???
 
 
+  def mergesort(seq: List[Int], start: Int, end: Int): (List[Int], Int, Int) = end - start match
+    case n if n < 1 => (seq, start, end)
+    case n => merge(mergesort(seq, start, start + (n/2)), mergesort(seq, start + 1 + (n/2), end))
 
-  def mergesort(seq: List[Int]): List[Int] = seq.length match
-    case n if n < 2 => seq
-    case _ =>
-      val l = seq.splitAt(seq.length/2)._1
-      val r = seq.splitAt(seq.length/2)._2
-      merge(mergesort(l), mergesort(r))
+  private def merge(l: (List[Int], Int, Int), r: (List[Int], Int, Int)): (List[Int], Int, Int) =
 
-  private def merge(l: List[Int], r: List[Int]): List[Int] =
-    var list = l.concat(r)
+    val startl = l._2
+    val startr = r._2
+    val end = r._3
+    var list = l._1.zipWithIndex.map((v, i) => i match
+      case n if n >= startr => r._1(n)
+      case _ => v)
 
-    var i = 0
-    var j = l.length
+    println("metto in ordine da " + startl + " a " + end)
 
-    while (j < list.length) {
+    var i = startl
+    var j = startr
+
+    while (j < end + 1) {
       if list(i) < list(j) then
         i = i + 1
       else
         for (k <- j until i by -1) {
+          println("swappare tra " + (k-1) + " e " + k)
           list = list.updated(k, list(k-1)).updated(k-1, list(k))
         }
         i = i + 1
         j = j + 1
     }
 
-    list
+    println("messo in ordine da " + startl + " a " + end)
 
+    (list, startl, end)
 }
--- src/test/scala/model/SortingTest.scala
+++ src/test/scala/model/SortingTest.scala
@@ -2,16 +2,20 @@ package model
 
 import model.SortingAlgorithms.*
 import model.StepsVisualizer.*
+import model.sortModel.{SelectableM, Selections}
 import model.sortModel.SortableFunctionalities.*
+import model.sortModel.SortOperation.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.language.postfixOps
 import scala.util.{Failure, Success}
 
 class SortingTest extends AnyFlatSpec with Matchers:
 
   given Comparable[Int] with
     override def compare(a: Int, b: Int): Boolean = a - b > 0
+  given Conversion[Steps[Int] with Selections[String, Int], SortOps[Steps[Int] with Selections[String, Int]]] = _ !
 
   "Bubble sort" should "work" in {
     val data = Seq(60, 20, 30, 40, 50, 10)
@@ -19,24 +23,17 @@ class SortingTest extends AnyFlatSpec with Matchers:
     val (result, string) = visualizeSteps(steps, data)
 
     print(string)
-
     result shouldBe Seq(10, 20, 30, 40, 50, 60)
   }
 
-//  "Selection sort" should "work" in {
-//    var mList = Selectable(5, 4, 2, 1, 3)
-//    for (i <- 0 to mList.length - 2) {
-//      mList = mList.select("min", i).get
-//      for (j <- i + 1 until mList.length) {
-//        mList = mList.compare(mList.getSelection("min"), j)(x => x.deselect("min").get.select("min", j).get)(x => x).get
-//      }
-//      mList = mList.swap(mList.getSelection("min"), i).get.deselect("min").get
-//    }
-//
-//    print(visualizeSteps(mList.steps, Seq(5, 4, 2, 1, 3))._2)
-//
-//    mList.data shouldBe Seq(1, 2, 3, 4, 5)
-//  }
+  "Selection sort" should "work" in {
+    val data = Seq(60, 20, 30, 40, 50, 10)
+    val steps = selectionSort(data)
+    val (result, string) = visualizeSteps(steps, data)
+
+    print(string)
+    result shouldBe Seq(10, 20, 30, 40, 50, 60)
+  }
 //
 //  "Insertion sort" should "work" in {
 //    var mList = Selectable(5, 4, 2, 1, 3)
@@ -53,9 +50,9 @@ class SortingTest extends AnyFlatSpec with Matchers:
 //
 //    mList.data shouldBe Seq(1, 2, 3, 4, 5)
 //  }
-//
-//  "Merge Sort" should "work" in {
-//    val list = List(500, 427, 223, 12, 34, 8, 3, 9, 94)
-//    val res = mergesort(list)
-//    res shouldBe List(3, 8, 9, 12, 34, 94, 223, 427, 500)
-//  }
+
+  "Merge Sort" should "work" in {
+    val list = List(4, 3, 2, 1)
+    val res = mergesort(list, 0, list.length-1)._1
+    res shouldEqual List(1, 2, 3, 4)
+  }
