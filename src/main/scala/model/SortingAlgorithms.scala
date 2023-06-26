ecb2712141cd3ef103f429ad5231235a6dd5fe4a
Alex Siroli <alex.siroli@studio.unibo.it>
2023-06-26 19:47:33 +0200

    refactoring merge sort with whileLoop and loopFor

--- src/main/scala/model/SortingAlgorithms.scala
+++ src/main/scala/model/SortingAlgorithms.scala
@@ -50,7 +50,7 @@ object SortingAlgorithms:
                   p3 <- j.previous.compare(j.value, j.previous -> "sel")(x =>
                     for p4 <- x.swap(x -> "sel", j.value)
                         p5 <- p4.select("sel", j.value)
-                    yield p5)(x => x)
+                    yield p5)(x => x) // x => x con break
                 yield p3
          p6 <- p2.deselect("sel")
     yield p6).get.steps
@@ -95,20 +95,16 @@ object SortingAlgorithms:
   (SortableM[T] with Steps[T] with Selections[String, T], Int, Int) =
     ((for p1 <- seq.select("i", start)
           p2 <- p1.select("j", mid + 1)
-          r <- p2.loopWhile(h => h -> "j" < end + 1 && h -> "i" != h -> "j")(g =>
-            for p1 <- g.compare(g -> "i", g -> "j")(x =>
-              for
-                i <- x.getSelection("i")
-                p2 <- x.select("i", i.get + 1)
-              yield p2)(x => for p2 <- x.iterate(x -> "j" until x -> "i" by -1)(
-              (k, t) => for p3 <- t.swap(k, k - 1)
-                yield p3)
-                                 i <- p2.getSelection("i")
-                                 p3 <- p2.select("i", i.get + 1)
-                                 j <- p3.getSelection("j")
-                                 p4 <- p3.select("j", j.get + 1)
-            yield p4
-            ) yield p1)
-          p3 <- r.deselect("i")
-          p4 <- p3.deselect("j")
-    yield p4).get, start, end)
+          p3 <- for p10 <- p2.whileLoop(h => h -> "j" < end + 1 && h -> "i" != h -> "j")
+                    p4 <- p10.compare(p10 -> "i", p10 -> "j")(x => x.select("i", p10 -> "i" + 1))(x =>
+                        for p5 <- for k <- x.loopFor(x -> "j" until x -> "i" by -1)
+                                      p6 <- k.previous.swap(k.value, k.value - 1)
+                                  yield p6
+                            p7 <- p5.select("i", p5 -> "i" + 1)
+                            p8 <- p7.select("j", p7 -> "j" + 1)
+                        yield p8)
+                yield p4
+          p8 <- p3.deselect("i")
+          p9 <- p8.deselect("j")
+    yield p9).get, start, end)
+
