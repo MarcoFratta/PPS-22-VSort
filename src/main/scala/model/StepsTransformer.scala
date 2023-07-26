1757d22defd81735e5da4eca675b1e28a10145af
Alex Siroli <alex.siroli@studio.unibo.it>
2023-07-26 17:06:28 +0200

    rmeoved match cases from StepsTransformer

--- src/main/scala/model/StepsTransformer.scala
+++ src/main/scala/model/StepsTransformer.scala
@@ -12,33 +12,51 @@ class StepsTransformer[T]:
 
   def getResult(steps: Seq[Step], seq: Seq[T]): Seq[T] = this.applySteps(steps, seq.map(i => newInfo(i)))
 
-  @tailrec
-  private def applySteps(steps: Seq[Step], seq: State): Seq[T] = steps match
-    case s :: t => applySteps(t, getNewSeq(s, seq))
-    case Nil => seq.map(e => e.value)
+  private def applySteps(steps: Seq[Step], seq: State): Seq[T] =
+    steps.foldLeft(seq)((currentSeq, step) => getNewSeq(step, currentSeq)).map(e => e.value)
 
-  private def getNewSeq(step: Step, seq: State): State = step match
-    case Step.Comparison(_, _) => seq
-    case s => stepFunction(s)(seq)
+  private def getNewSeq[K](step: Step, seq: State): State = {
+    val isComparison = step.isInstanceOf[Comparison[K]]
+    if (isComparison) seq else stepFunction(step)(seq)
+  }
 
-  private def stepFunction(step: Step): State => State = step match
-    case Step.Swap(a: Int, b: Int) =>
-      seq => seq.updated(a, seq(a).changeValue(seq(b).value)).updated(b, seq(b).changeValue(seq(a).value))
-    case Step.Selection(s: String, a: Int) => seq => stepFunction(Step.Deselection(s))(seq) match
-      case n if a >= 0 && a < n.size => n.updated(a, n(a).select(s))
-      case n => n
-    case Step.Deselection(s) => seq => seq.map(e => if e.label == Option(s) then deselect(e) else e)
-    case Step.Comparison(a: Int, b: Int) => seq => seq.updated(a, compare(seq(a))).updated(b, compare(seq(b)))
-    case Step.Divide(a: Int, b: Int) => seq => seq.zipWithIndex.map((e, i) => if i < a || i > b then hide(e) else show(e))
+  private def stepFunction(step: Step): State => State =
+    if (step.isInstanceOf[Swap[Int]])
+      val swapStep = step.asInstanceOf[Swap[Int]]
+      seq =>
+        seq.updated(swapStep.a, seq(swapStep.a).changeValue(seq(swapStep.b).value))
+          .updated(swapStep.b, seq(swapStep.b).changeValue(seq(swapStep.a).value))
+    else if (step.isInstanceOf[Selection[String, Int]])
+      val selectionStep = step.asInstanceOf[Selection[String, Int]]
+      seq =>
+        val deselected = stepFunction(Deselection(selectionStep.s))(seq)
+        if (selectionStep.a >= 0 && selectionStep.a < deselected.size)
+          deselected.updated(selectionStep.a, deselected(selectionStep.a).select(selectionStep.s))
+        else deselected
+    else if (step.isInstanceOf[Deselection[String]])
+      val deselectionStep = step.asInstanceOf[Deselection[String]]
+      seq => seq.map(e => if (e.label == Option(deselectionStep.s)) deselect(e) else e)
+    else if (step.isInstanceOf[Comparison[Int]])
+      val comparisonStep = step.asInstanceOf[Comparison[Int]]
+      seq =>
+        seq.updated(comparisonStep.a, compare(seq(comparisonStep.a)))
+          .updated(comparisonStep.b, compare(seq(comparisonStep.b)))
+    else if (step.isInstanceOf[Divide[Int]])
+      val divideStep = step.asInstanceOf[Divide[Int]]
+      seq =>
+        seq.zipWithIndex.map((e, i) =>
+          if (i < divideStep.start || i > divideStep.stop) hide(e) else show(e))
+    else
+      throw new IllegalArgumentException("Unknown step type")
 
   def getString(steps: Seq[Step], seq: Seq[T]): String = this.getStepsString(steps, seq.map(i => newInfo(i)))
 
-  private def getStepsString(steps: Seq[Step], map: State): String = steps match
-    case s :: t => getSeq(s, map).toString + " -> " + s.toString + "\n" + getStepsString(t, getNewSeq(s, map))
-    case Nil => "Array finale -> " + map
+  private def getStepsString(steps: Seq[Step], map: State): String =
+    steps.foldLeft("Array finale -> " + map) ((acc, step) =>
+      val seqString = getSeq(step, map).toString
+      acc + s" -> $seqString\n" + getNewSeq(step, map))
 
   private def getSeq(step: Step, map: State): State = stepFunction(step)(map)
-
 
   def getSeqList(steps: Seq[Step], seq: Seq[T]): Seq[State] =
     getSeqList(steps, seq.map(i => newInfo(i)), Seq(seq.map(i => newInfo(i))))
@@ -46,9 +64,7 @@ class StepsTransformer[T]:
         case Seq(a, b) if a == b => None
         case Seq(a, _) => Some(a)}.toSeq
 
-  @tailrec
-  private def getSeqList(steps: Seq[Step],
-                         last: State,
-                         seq: Seq[State]): Seq[State] = steps match
-    case s :: t => getSeqList(t, getNewSeq(s, last), seq.appended(getSeq(s, last)))
-    case Nil => seq
+  private def getSeqList(steps: Seq[Step], last: State, seq: Seq[State]): Seq[State] =
+    steps.foldLeft(Seq(last)) ((acc, step) => acc :+ getNewSeq(step, acc.last)).tail
+
+
