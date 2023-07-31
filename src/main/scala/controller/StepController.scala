da154364e4be18a98066e81e569d02c2f67ea148
Alice Mastrilli <alice.mastrilli@studio.unibo.it>
2023-07-31 12:56:53 +0200

    changed return type of getElements in StepController

--- src/main/scala/controller/StepController.scala
+++ src/main/scala/controller/StepController.scala
@@ -1,6 +1,5 @@
 package controller
 import com.raquo.laminar.api.L.Var
-import controller.StepController.{seq, steps}
 import model.*
 import model.seqProperties.*
 import model.SortingAlgorithms.mergeSort
@@ -11,38 +10,14 @@ import view.{GraphFunctions, View}
 import scala.collection.immutable.Map
 
 object StepController:
-  
-  given Generable[Int] = x => x.toInt
+  trait SeqProp[T]:
+    def getElements: Seq[State[ElementInfo[T]]]
 
-  case class RangeGaussian[T: Generable](mi: Int, ma: Int)
-    extends GaussianGen[T](75, 25)
-      with Shifted[T](mi, ma)
-
-  import model.sortModel.SortOperations.given
-
-  
-  var seq: Seq[Int] = RangeGaussian(5,10).generateAll(0 to 100).toList.sortWith((a, b) => a._1 <= b._1).map(x => x._2)
-  var steps: Seq[Step] = mergeSort(seq)
-  var example: Seq[Seq[ElementInfo[Int]]] = StepsTransformer[Int].getSeqList(steps, seq)
-
-
-  trait SeqProp:
-    def getElements: Seq[Seq[ElementInfo[Int]]]
-
-  case class SeqProperties(prop: Properties) extends SeqProp:
+  case class SeqProperties(prop: Properties) extends SeqProp[Int]:
     private def findParamFromName(name: String): Params =
       prop.paramMap.filter(a => a._1.toString equals name).toList.head._1
-    override def getElements: Seq[Seq[ElementInfo[Int]]] =
-
-      //0 to prop.map(findParamFromName("Size"))
-      var seq: Seq[Int] = prop.distribution.generator(prop.paramMap).generateAll(0 to prop.paramMap(findParamFromName("Size"))).
-        toList.sortWith((a, b) => a._1 <= b._1).map(x => x._2)
-
-      //var steps: Seq[Step] = prop.alg.execute(seq).map(a => a.get)
-      var example: Seq[Seq[ElementInfo[Int]]] = prop.alg.execute(seq).map(a => a.get)
-      println("alg" + prop.alg.name)
-      example
-      //example
-
-      //view.setSeqList(example)
+    override def getElements: Seq[State[ElementInfo[Int]]] =
+      val seq: Seq[Int] = prop.distribution.generator(prop.paramMap).generateAll(0 to 
+        prop.paramMap(findParamFromName("Size"))).toList.sortWith((a, b) => a._1 <= b._1).map(x => x._2)
+      prop.alg.execute(seq)
 
