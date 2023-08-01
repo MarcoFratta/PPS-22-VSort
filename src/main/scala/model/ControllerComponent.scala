600636ccc6fdb2e02ae439d363c9643d77d4f21f
marco <marco.frattarola@studio.unibo.it>
2023-08-01 20:37:16 +0200

    Adding ControllerComponent and ModelComponent

---
+++ src/main/scala/model/ControllerComponent.scala
@@ -0,0 +1,34 @@
+package model
+
+import controller.Properties
+import controller.StepController.SeqProperties
+import model.*
+import model.ModelComponent.Model
+import view.*
+
+object ControllerComponent:
+  type Requirements = Model.Provider with ViewComponent.Provider
+
+  trait Controller[T]:
+    def update(p: T): Unit
+
+  trait Provider:
+    val controller: Controller[Properties with IntTypes]
+
+  trait Component:
+    c: Requirements =>
+
+    class ControllerImpl extends Controller[Properties with IntTypes]:
+
+      override def update(p: Properties with IntTypes): Unit =
+        val r = SeqProperties(p).getElements
+        println(f"res  $r")
+        if checkField(p) then c.view.update(r)
+
+
+      private def checkField(properties: Properties with IntTypes): Boolean =
+        properties.algorithm != null && properties.distribution != null && properties.params.nonEmpty
+
+
+  trait Interface extends Provider with Component:
+    self: Requirements =>
---
+++ src/main/scala/model/ModelComponent.scala
@@ -0,0 +1,48 @@
+package model
+
+import controller.*
+import model.*
+import model.SortingAlgorithms.*
+import model.seqProperties.*
+import model.seqProperties.Distributions.*
+import view.*
+
+
+object ModelComponent:
+
+  object Model:
+    trait Model extends ModelTypes with Algorithms with Distributions
+
+    trait Provider:
+      val model: Model with IntTypes
+
+    trait Component:
+      case class ModelImpl() extends Model with IntTypes:
+
+        import model.Params.*
+        import model.SortingAlgorithms.{*, given}
+
+        given Generable[Int] = x => x.toInt
+
+        override def algorithms: Set[Algorithm[ValType, ResultType] with HasName] =
+          Set(AlgorithmFactory.intAlgorithm(bubbleSort, "Bubble sort"),
+            AlgorithmFactory.intAlgorithm(mergeSort, "Merge sort"),
+            AlgorithmFactory.intAlgorithm(insertionSort, "Insertion sort"),
+            AlgorithmFactory.intAlgorithm(quickSort, "Quicksort"),
+            AlgorithmFactory.intAlgorithm(heapSort, "Heap sort"))
+
+        override def distributions: Set[Distribution[ParamsType, ValType] with HasName] =
+
+          Set(DistributionFactory(p =>
+            given Conversion[Params, Int] = x => p(x)
+
+            GaussianDistribution(Size / 2, Std, 1, 10000),
+            Set(Size, Std), "Gaussian"),
+            DistributionFactory(p =>
+              given Conversion[Params, Int] = x => p(x)
+
+              UniformDistribution(Min, Max, DuplicatesPercentage),
+              Set(Min, Max, DuplicatesPercentage), "Uniform"))
+
+
+    trait Interface extends Provider with Component
