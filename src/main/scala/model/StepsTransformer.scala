22737f38b37c50767675754d9fd15840909060cf
Alex Siroli <alex.siroli@studio.unibo.it>
2023-08-04 13:09:54 +0200

    bug fixes

--- src/main/scala/model/StepsTransformer.scala
+++ src/main/scala/model/StepsTransformer.scala
@@ -32,23 +32,6 @@ class StepsTransformer[T]:
     case Step.Divide(a: Int, b: Int) => seq
       => seq.zipWithIndex.map((e, i) => if i < a || i > b then hide(e) else show(e))
 
-  def getString(steps: Seq[Step], seq: Seq[T]): String =
-    this.getStepsString(steps, seq.map(i => newInfo(i)))
-
-  private def getStepsString(steps: Seq[Step], map: State): String =
-    steps.foldLeft("Array finale -> " + map) ((acc, step) =>
-      val seqString = getSeq(step, map).toString
-      acc + s" -> $seqString\n" + getNewSeq(step, map))
-
-  private def getSeq(step: Step, map: State): State = stepFunction(step)(map)
-
   def getSeqList(steps: Seq[Step], seq: Seq[T]): Seq[State] =
-    getSeqList(steps, seq.map(i => newInfo(i)), Seq(seq.map(i => newInfo(i))))
-      .sliding(2).flatMap {
-        case Seq(a, b) if a == b => None
-        case Seq(a, _) => Some(a)}.toSeq
-
-  private def getSeqList(steps: Seq[Step], last: State, seq: Seq[State]): Seq[State] =
-    steps.foldLeft(Seq(last)) ((acc, step) => acc :+ getNewSeq(step, acc.last)).tail
-
-
+    val state: State = seq.map(i => newInfo(i))
+    steps.foldLeft(Seq(state)) ((acc, step) => acc :+ getNewSeq(step, acc.last))
